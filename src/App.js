d568e857fa89a74907bf3787ea6522957c87e6b5
Ikarosv <ikarovieiraa@gmail.com>
2022-11-03 15:12:20 -0300

    feat: adicionado o not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
@@ -41,6 +42,9 @@ class App extends React.Component {
             exact
             path="/profile/edit"
             render={ () => <ProfileEdit /> }
+          />
+          <Route
+            render={ () => <NotFound /> }
           />
         </Switch>
       </BrowserRouter>
