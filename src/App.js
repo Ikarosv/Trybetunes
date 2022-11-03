bfbe14106a81b65ae1060675351d13b484c467e6
Ikarosv <ikarovieiraa@gmail.com>
2022-11-03 14:51:48 -0300

    feat: adiciona rota home que renderiza ologin
    adicionado a rota "/" que renderiza a page Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => <Login /> }
+          />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div ata-testid="page-login">Login</div>
+    );
+  }
+}
