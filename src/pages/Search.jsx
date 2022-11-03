9549c3fab8f14bb68e237559ba130b803e94d894
Ikarosv <ikarovieiraa@gmail.com>
2022-11-03 17:50:54 -0300

    feat: adiciona input de pesquisa

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const MIN_LENGTH_NAME = 2;
 export default class Search extends Component {
+  state = {
+    artistName: '',
+    disabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { artistName } = this.state;
+      this.setState({
+        disabled: artistName.length < MIN_LENGTH_NAME,
+      });
+    });
+  };
+
   render() {
+    const { artistName, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            name="artistName"
+            value={ artistName }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            placeholder="Artist Name"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disabled }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
