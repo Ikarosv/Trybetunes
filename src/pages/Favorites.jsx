1f5acfc521e0d3f43b0a64943b1808be883b73c5
Ikarosv <ikarovieiraa@gmail.com>
2022-11-09 11:14:38 -0300

    cria página de favoritos

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,7 +3,11 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import MusicCard from '../components/MusicCard';
+import '../components/styles/scss/Favorites.scss';
+import Lines from '../components/styles/styledComponents/Lines';
 
+const COLOR_LINES_TOP_LEFT = '#00D5E2';
+const COLOR_LINES_BOTTOM_RIGHT = '#003BE5';
 export default class Favorites extends Component {
   state = {
     favorites: [],
@@ -42,16 +46,35 @@ export default class Favorites extends Component {
     return (
       <div data-testid="page-favorites">
         <Header />
-        {
-          loading ? <Loading /> : (
-            favorites.map((music, index) => (<MusicCard
-              key={ music.trackName + index }
-              { ...music }
-              isFavorite
-              attFavorite={ this.reloadFavoritesSongs }
-            />))
-          )
-        }
+        <section className="pageFavorites">
+          <section className="horizontalHeader bgGradient">
+            <h1>Musicas Favoritas</h1>
+            <Lines
+              className="max-height-and-width top-left"
+              blur
+              lineColor={ COLOR_LINES_TOP_LEFT }
+            />
+            <Lines
+              className="max-height-and-width bottom-right-splited"
+              blur
+              lineColor={ COLOR_LINES_BOTTOM_RIGHT }
+            />
+            <span className="big-outlined-circle bottom-left-big-circle" />
+            <span className="big-filled-circle top-right" />
+          </section>
+          <section className="favoritesMusics">
+            {
+              loading ? <Loading /> : (
+                favorites.map((music, index) => (<MusicCard
+                  key={ music.trackName + index }
+                  { ...music }
+                  isFavorite
+                  attFavorite={ this.reloadFavoritesSongs }
+                />))
+              )
+            }
+          </section>
+        </section>
       </div>
     );
   }
